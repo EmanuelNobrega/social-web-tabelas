1034a429f69a02e7dbaae40640a7f6f4065f5136
Emanuel Nóbrega <egn1995@hotmail.com>
2021-09-06 01:21:50 -0300

    Initial commit

---
+++ src/app/shared/model/USUARIOS.ts
@@ -0,0 +1,26 @@
+export const USUARIOS = [
+  {
+    nome: 'Gustavo',
+    cpf: '123',
+    idade: 34,
+    telefone: '8391234567'
+  },
+  {
+    nome: 'Wagner',
+    cpf: '124',
+    idade: 32,
+    telefone: '08391234567'
+  },
+  {
+    nome: 'Diniz',
+    cpf: '125',
+    idade: 30,
+    telefone: '12345678'
+  },
+  {
+    nome: 'Emanuel',
+    cpf: '195',
+    idade: 25,
+    telefone: '123456789'
+  }
+];
